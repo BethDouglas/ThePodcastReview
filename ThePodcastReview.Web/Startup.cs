522c82cde2f43f3d3952ef436385a4865c0727bf
Beth Douglas <bethDdouglas@gmail.com>
2017-12-05 14:37:30 -0500

    Add project files.

---
+++ ThePodcastReview.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThePodcastReview.Web.Startup))]
+namespace ThePodcastReview.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
