6e779ce77d8db2b45515a861a8d52060f6536942
Beth Douglas <bethDdouglas@gmail.com>
2017-12-05 15:17:33 -0500

    Finished Part 4

---
+++ ThePodcastReview.Models/ReviewListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePodcastReview.Models
+{
+    public class ReviewListItem
+    {
+        public int ReviewId { get; set; }
+
+        [Display(Name = "Podcast")]
+        public string PodcastTitle { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Reviewer { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString() => PodcastTitle;
+    }
+}
