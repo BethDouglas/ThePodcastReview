a93256f296dcf16e0e586a309055a6a7a57bd2a3
Beth Douglas <bethDdouglas@gmail.com>
2017-12-05 14:52:27 -0500

    Moved Identity Models

--- ThePodcastReview.Web/App_Start/IdentityConfig.cs
+++ ThePodcastReview.Web/App_Start/IdentityConfig.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using ThePodcastReview.Data;
 using ThePodcastReview.Web.Models;
 
 namespace ThePodcastReview.Web
--- ThePodcastReview.Web/Controllers/AccountController.cs
+++ ThePodcastReview.Web/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using ThePodcastReview.Data;
 using ThePodcastReview.Web.Models;
 
 namespace ThePodcastReview.Web.Controllers
