2f76a1d30d53cd07bfc317db4bcea3113fe30223
Beth Douglas <bethDdouglas@gmail.com>
2017-12-05 15:10:25 -0500

    Finished Part 3

---
+++ ThePodcastLibrary.Data/Review.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePodcastLibrary.Data
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string PodcastTitle { get; set; }
+
+        [MaxLength(200)]
+        public string Episode { get; set; }
+
+        [Range(1,5)]
+        public int Rating { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Content { get; set; }
+
+        public string FavEpisodes { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
