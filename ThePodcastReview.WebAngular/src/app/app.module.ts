1a59b8b69466805fe584e8a4dae96a7904c5a795
Beth Douglas <bethDdouglas@gmail.com>
2017-12-19 10:01:04 -0500

    Finished Angular Module 2.

--- ThePodcastReview.WebAngular/src/app/app.module.ts
+++ ThePodcastReview.WebAngular/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material';
+import { 
+         MatToolbarModule,
+         MatToolbar,
+         MatButtonModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-
-import '~@angular/material/prebuilt-themes/deeppurple-amber.css';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import '~@angular/material/prebuilt-themes/deeppurple-amber.css';
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
