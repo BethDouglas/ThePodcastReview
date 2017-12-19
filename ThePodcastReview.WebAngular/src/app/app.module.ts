14e3034bf8ec507ef90da46d91c6b0ed2820f9fa
Beth Douglas <bethDdouglas@gmail.com>
2017-12-19 17:06:27 -0500

    Register Working.

--- ThePodcastReview.WebAngular/src/app/app.module.ts
+++ ThePodcastReview.WebAngular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -23,6 +24,7 @@ import { RegistrationComponent } from './components/registration/registration.co
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     MatToolbarModule,
--- ThePodcastReview.WebAngular/src/app/components/registration/registration.component.ts
+++ ThePodcastReview.WebAngular/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -10,22 +10,25 @@ export class RegistrationComponent implements OnInit {
 
   private _registerForm: FormGroup;
 
-  constructor(private _form: FormBuilder) {
+  constructor(private _form: FormBuilder) { 
     this.createForm();
-   }
+    console.log("Constructor:", this._registerForm.value);
+  }
 
   ngOnInit() {
+    console.log("ngOnInit", this._registerForm.value);
   }
 
-  createForm() {
+  createForm(){
     this._registerForm = this._form.group({
-      email: new FormControl,
-      password: new FormControl,
+      email: new FormControl('', Validators.min(5)),
+      password: new FormControl('', Validators.min(10)),
       confirmPassword: new FormControl
     });
+    console.log("createForm:", this._registerForm.value);
   }
 
-onsubmit() {
-  console.log(this._registerForm.value);
+  onSubmit(){
+    console.log("onSubmit:", this._registerForm.value);
   }
 }
