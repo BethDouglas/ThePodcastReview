472f1aa43f499e178628b2a746ff4ee7522abe31
Beth Douglas <bethDdouglas@gmail.com>
2017-12-19 15:55:51 -0500

    Module 3 before Cors.

--- ThePodcastReview.WebAngular/src/app/app.module.ts
+++ ThePodcastReview.WebAngular/src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { 
          MatToolbarModule,
          MatToolbar,
-         MatButtonModule
+         MatButtonModule,
+         MatFormFieldModule,
+         MatInputModule
+
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
