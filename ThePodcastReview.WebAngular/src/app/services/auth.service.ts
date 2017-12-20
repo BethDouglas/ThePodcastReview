21d9d4aeb6117fc38f07b5e01a09297dd48c8543
Beth Douglas <bethDdouglas@gmail.com>
2017-12-20 10:44:47 -0500

    Start of Part 8.

--- ThePodcastReview.WebAngular/src/app/services/auth.service.ts
+++ ThePodcastReview.WebAngular/src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/REgisterUser';
 import { HttpClient } from '@angular/common/http';
-import { Token } from '@angular/compiler';
+import { Token } from '../models/Token';
+import { Router } from '@angular/router';
+
 
 const Api_Url = 'http://localhost:60311';
 
@@ -9,7 +11,7 @@ const Api_Url = 'http://localhost:60311';
 export class AuthService {
 
 
-  constructor( private _http: HttpClient) { }
+  constructor( private _http: HttpClient, private _router: Router) { }
 
   register(regUserData: RegisterUser){
     return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
@@ -21,7 +23,7 @@ export class AuthService {
 
     return this._http.post(`${Api_Url}/token`, str).subscribe((token: Token) => {
       localStorage.setItem('id_token', token.access_token);
-      console.log(token.access_token);
+      this._router.navigate(['/']);
     });
 
   }
