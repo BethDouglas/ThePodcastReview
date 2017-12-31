9c24afa9611ec561ef27e243e24f97106931cda8
Beth Douglas <bethDdouglas@gmail.com>
2017-12-30 22:41:04 -0500

    added migration

---
+++ ThePodcastReview.Data/Migrations/201712310338595_updatingAgain.cs
@@ -0,0 +1,16 @@
+namespace ThePodcastReview.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatingAgain : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
